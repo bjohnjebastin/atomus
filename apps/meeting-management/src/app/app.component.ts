31625343c44160b9d3de28d59f9bbca38d677e16
bjohnjebastin <bjohnjebastin@gmail.com>
2021-08-07 20:54:15 +0100

    initial commit

---
+++ apps/meeting-management/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atomus-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'meeting-management';
+}
